db54ac50510cbe086564e02c27b9759b1fb975a6
KSuga-tech <ksuga.5555@gmail.com>
2020-12-30 17:20:09 +0900

    「答えの表示」ボタン作成
    コメントアウトの表示

--- app/assets/javascripts/contents.js
+++ app/assets/javascripts/contents.js
@@ -8,44 +8,55 @@
 // })
 
 window.addEventListener("load", function () {
+
+  // プリントの印刷機能
   let printBtn = document.getElementById("print");
   printBtn.addEventListener("click", function() {
+
+    // 印刷ボタンを押すとプリント部分以外が非表示になる。
     let hideLeft = document.querySelector(".leftSide");
     let hideHeader = document.querySelector(".header");
     let changeRight = document.querySelector(".rightSide");
     let hide = document.getElementsByClassName("hide");
-      // hideLeft.style.display = 'none';
-      // classList.add(hide[0].classList);
-  console.log(hideLeft.classList);
-  console.log(hide[0].classList);
-  hideLeft.classList.add(hide[0].classList);
-  hideHeader.style.display = 'none';
-  changeRight.style.width = '100%';
-  window.print();
-  hideLeft.classList.remove(hide[0].classList);
-  hideHeader.style.display = 'flex';
-  changeRight.style.width = 'calc( 100% - 300px )';
+    hideLeft.classList.add(hide[0].classList);
+    hideHeader.style.display = 'none';
+    changeRight.style.width = '100%';
+
+    // プリントダイアログが表示される。
+    window.print();
+
+    // プリントダイアログを終了すると、元のレイアウトに戻る。
+    hideLeft.classList.remove(hide[0].classList);
+    hideHeader.style.display = 'flex';
+    changeRight.style.width = 'calc( 100% - 300px )';
   });
 
+  // 問題の生成機能,「答えの表示」ボタン表示
   let btn = document.getElementById("submit");
   btn.addEventListener("click", function () {
+    let answerBtn = document.querySelector(".keisanSheet__answer");
+    answerBtn.style.display = 'inline-block';
     let num1 = document.getElementById("num1").value;
     let symbol = document.getElementById("calc_symbol").value;
     let num2 = document.getElementById("num2").value;
     let sheet = document.getElementById("main");
     sheet.innerHTML = '';
 
+    // 問題を２列にする。
     for (let x = 0; x < 2; x++){
      let newElement = document.createElement("div");
       let idNumber = "mondai" + x
       newElement.setAttribute("class", "mondai", "id", idNumber);
       sheet.appendChild(newElement);
 
+      // 問題は１列に10問ずつ作られる。
       for (let i = 1; i < 11; i++) {
         let number = x * 10 + i;
         let rand1 = Math.floor( Math.random() * (10 ** (num1 - 1) * 9) + 10 ** (num1 - 1));
         let rand2 = Math.floor( Math.random() * (10 ** (num2 - 1) * 9) + 10 ** (num2 - 1));
         let rand3 = 0
+
+        // 引き算の時、答えが必ず0以上になるようにする。
         if (symbol == "-") {
           if (rand1 < rand2) {
             rand3 = rand2;
@@ -54,6 +65,7 @@ window.addEventListener("load", function () {
           }
         }
 
+        // 解答の生成
         let answers = 0
         if (symbol == '+') {
           answers = rand1 + rand2;
@@ -64,7 +76,8 @@ window.addEventListener("load", function () {
         } else {
           answers = rand1 / rand2;
         }
- 
+
+        // 問題と解答を表示する。
         let html = `
           <div class="mondai__block">
             <div class="mondai__number">
@@ -76,7 +89,6 @@ window.addEventListener("load", function () {
           </div>
           `
         newElement.innerHTML += html;
-        // console.log(rand1 + symbol + rand2);
       }
     }
   });
